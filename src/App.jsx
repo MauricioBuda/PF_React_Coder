9b61042cad3d60c669459eaf2e5b99c27b32fcb6
Mauricio <mauricio.ariel.buda@gmail.com>
2023-07-23 21:10:02 -0300

    vinculación con Vercel

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,32 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import ItemDetailContainer from "./components/ItemDetailContainer/ItemDetailContainer"
+import ItemListContainer from "./components/ItemListContainer/ItemListContainer"
+import Navbar from "./components/NavBar/NavBar"
+import Cart from "./components/Cart/Cart"
+import Checkout from "./components/Checkout/Checkout"
+
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { CartProvider } from "./context/CartContext"
+import { NotificationProvider } from "./notification/NotificationService"
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <BrowserRouter>
+        <NotificationProvider>
+          <CartProvider>
+            <Navbar />
+            <Routes>
+              <Route path='/' element={<ItemListContainer greeting={'Todos los productos'}/>}/>
+              <Route path='/category/:categoryId' element={<ItemListContainer greeting={'Filtrados'}/>}/>
+              <Route path='/item/:itemId' element={<ItemDetailContainer/>}/>
+              <Route path='/cart' element={<Cart />}/>
+              <Route path='/checkout' element={<Checkout />}/>
+              <Route path='*' element={<h1>URL INCORRECTO</h1>}/>
+            </Routes>
+          </CartProvider>
+        </NotificationProvider>
+      </BrowserRouter>
     </>
   )
 }
-
 export default App
